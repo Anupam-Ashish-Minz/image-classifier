f4955c3a6bd4b4027a2692dff4e260b24d719613
Anupam Ashish Minz <anupamminz300am@gmail.com>
2022-11-09 13:05:55 +0530

    made canvas

--- img-classifier-client/src/routes/+page.svelte
+++ img-classifier-client/src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let draw_mode = false;
+	let mouse_pos = {
+		x: 0,
+		y: 0
+	};
+</script>
+
+<h1>Draw an image</h1>
+<p>mouse position {mouse_pos.x} {mouse_pos.y}</p>
+
+<canvas
+	class="canvas"
+	on:mouseup={() => {
+		draw_mode = false;
+	}}
+	on:mousedown={() => {
+		draw_mode = true;
+	}}
+	on:mousemove={(event) => {
+		if (draw_mode)
+			mouse_pos = {
+				x: event.clientX,
+				y: event.clientY
+			};
+	}}
+	width="560px"
+	height="560px"
+/>
+
+<style>
+	.canvas {
+		background-color: gray;
+		width: 560px;
+		height: 560px;
+	}
+</style>
