01ba38cd972e8ad66625f446e6da365f2648691e
Anupam Ashish Minz <anupamminz300am@gmail.com>
2022-11-09 23:40:46 +0530

    client image connected to main server

--- img-classifier-client/src/routes/+page.svelte
+++ img-classifier-client/src/routes/+page.svelte
@@ -5,7 +5,9 @@
 	// pixel size
 	const px = 10;
 
-	let image_values = new Int8Array(784).fill(0);
+	let ai_guess: number = -1;
+
+	let image_values = new Uint8Array(784).fill(0);
 
 	let canvas: HTMLCanvasElement;
 	let ctx: CanvasRenderingContext2D | null;
@@ -19,8 +21,8 @@
 	function make_background() {
 		ctx = canvas.getContext('2d');
 		if (ctx) {
-			ctx.fillStyle = '#ffffff';
-			ctx.strokeStyle = '#000000';
+			ctx.strokeStyle = '#ffffff';
+			ctx.fillStyle = '#000000';
 			for (let i = 0; i < block_count; i++) {
 				for (let j = 0; j < block_count; j++) {
 					ctx.strokeRect(i * px, j * px, px, px);
@@ -33,19 +35,19 @@
 	function draw_pixel({ x, y }: { x: number; y: number }) {
 		const index = Math.floor(x / px) + block_count * Math.floor(y / px);
 		if (ctx) {
-			ctx.strokeStyle = '#aaa';
-			ctx.fillStyle = '#000';
+			ctx.fillStyle = '#fff';
+			ctx.strokeStyle = '#000';
 			ctx.fillRect(x - (x % px), y - (y % px), px, px);
 			ctx.strokeRect(x - (x % px), y - (y % px), px, px);
 			image_values[index] = 255;
 		}
 	}
 
-	function draw(event: MouseEvent & { currentTarget: EventTarget & HTMLCanvasElement }) {
+	function draw(e: MouseEvent & { currentTarget: EventTarget & HTMLCanvasElement }) {
 		const canvas_rect = canvas.getBoundingClientRect();
 		mouse_pos = {
-			x: Math.floor(event.clientX - canvas_rect.left),
-			y: Math.floor(event.clientY - canvas_rect.top)
+			x: Math.floor(e.clientX - canvas_rect.left),
+			y: Math.floor(e.clientY - canvas_rect.top)
 		};
 		// draw_pixel({ x: mouse_pos.x, y: mouse_pos.y - px });
 		draw_pixel({ x: mouse_pos.x, y: mouse_pos.y + px });
@@ -53,6 +55,30 @@
 		draw_pixel({ x: mouse_pos.x + px, y: mouse_pos.y });
 
 		draw_pixel({ x: mouse_pos.x, y: mouse_pos.y });
+	}
+
+  function resetImage(e: MouseEvent&{ currentTarget: EventTarget&HTMLButtonElement; }) {
+    e.preventDefault();
+    image_values = image_values.fill(0);
+    make_background();
+    ai_guess = -1;
+  }
+
+	function sendToAI(e: MouseEvent & { currentTarget: EventTarget & HTMLButtonElement }) {
+		e.preventDefault();
+
+		fetch('http://localhost:4000/ai', {
+			headers: {
+				'Content-Type': 'application/octet-stream'
+			},
+			method: 'post',
+			body: image_values
+		})
+			.then((data) => data.blob())
+      .then(data => data.arrayBuffer())
+      .then(data => new Uint8Array(data))
+      .then(data => ai_guess = data[1])
+			.catch((err) => console.error(err));
 	}
 
 	onMount(() => {
@@ -63,26 +89,43 @@
 <h1>Draw an Digit</h1>
 <p>mouse position {mouse_pos.x} {mouse_pos.y}</p>
 
-<canvas
-	class="canvas"
-	bind:this={canvas}
-	on:mouseup={() => {
-		draw_mode = false;
-	}}
-	on:mousedown={() => {
-		draw_mode = true;
-	}}
-	on:mousemove={(event) => {
-		if (draw_mode) draw(event);
-	}}
-	on:click={(event) => {
-		draw(event);
-	}}
-	width={block_count * px}
-	height={block_count * px}
-/>
+<div class="container">
+	<canvas
+		class="canvas"
+		bind:this={canvas}
+		on:mouseup={() => {
+			draw_mode = false;
+		}}
+		on:mousedown={() => {
+			draw_mode = true;
+		}}
+		on:mousemove={(event) => {
+			if (draw_mode) draw(event);
+		}}
+		on:click={(event) => {
+			draw(event);
+		}}
+		width={block_count * px}
+		height={block_count * px}
+	/>
+	<div>
+		<button on:click={e => resetImage(e)}>reset</button>
+		<button on:click={(e) => sendToAI(e)}>check with AI</button>
+	</div>
+
+	<div>
+		<h2>What the AI thinks</h2>
+		{#if ai_guess > -1}
+			<h2>{ai_guess}</h2>
+		{/if}
+	</div>
+</div>
 
 <style>
+  .container {
+    display: grid;
+    grid-template-columns: auto auto auto;
+  }
 	.canvas {
 		background-color: gray;
 		border: 1px solid black;
