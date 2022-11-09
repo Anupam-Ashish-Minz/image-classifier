c5a90f48f30ac799923bf654ea6f4bca040e7248
Anupam Ashish Minz <anupamminz300am@gmail.com>
2022-11-09 13:23:48 +0530

    made the canvas into pixel grid

--- img-classifier-client/src/routes/+page.svelte
+++ img-classifier-client/src/routes/+page.svelte
@@ -1,9 +1,32 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+
+	const block_count = 28;
+  // pixel size
+	const px = 20;
+
+	let canvas: HTMLCanvasElement;
+	let ctx;
+
 	let draw_mode = false;
 	let mouse_pos = {
 		x: 0,
 		y: 0
 	};
+
+	onMount(() => {
+		ctx = canvas.getContext('2d');
+		if (ctx) {
+			ctx.fillStyle = '#ffffff';
+			ctx.strokeStyle = '#000000';
+			for (let i = 0; i < block_count; i++) {
+				for (let j = 0; j < block_count; j++) {
+					ctx.strokeRect(i * px, j * px, px, px);
+					ctx.fillRect(i * px, j * px, px, px);
+				}
+			}
+		}
+	});
 </script>
 
 <h1>Draw an image</h1>
@@ -11,6 +34,7 @@
 
 <canvas
 	class="canvas"
+	bind:this={canvas}
 	on:mouseup={() => {
 		draw_mode = false;
 	}}
@@ -31,7 +55,6 @@
 <style>
 	.canvas {
 		background-color: gray;
-		width: 560px;
-		height: 560px;
+		border: 1px solid black;
 	}
 </style>
